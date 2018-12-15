8bf0e632c5cdb59fad31fd253f721fc3b47d3200
Abhiraj Pundir <pundirabhiraj@gmail.com>
2018-12-15 09:11:15 -0800

    Building a custom authentication Backend

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -86,6 +86,10 @@ DATABASES = {
     }
 }
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend',
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
